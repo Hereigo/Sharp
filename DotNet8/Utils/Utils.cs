eef546f65d0eac72f98662e3c13a0cf8f540856d
ELEKS-SOFTWARE\andrii.plakhtii <andrii.plakhtii@eleks.com>
2024-08-23 21:36:39 +0300

    ui update

--- DotNet8/Utils/Utils.cs
+++ DotNet8/Utils/Utils.cs
@@ -1,24 +1,10 @@
-﻿using DotNet8.Models;
-
-namespace DotNet8.Utils
+﻿namespace DotNet8.Utils
 {
     public static class Utils
     {
         public static int GetMaxDayOfTheMonth(DateTime date)
         {
             return new DateTime(date.Year, date.Month, 1).AddMonths(1).AddDays(-1).Day;
-        }
-
-        public static List<CalEvent> GetCalEventVm(List<CalEvent>? calEvent, int day)
-        {
-            if (calEvent?.Count > 0)
-            {
-                return calEvent;
-            }
-            else
-            {
-                return new List<CalEvent> { };
-            }
         }
     }
 }
