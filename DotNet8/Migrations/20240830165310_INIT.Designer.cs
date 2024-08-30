e1ae18b0620d2c6a3fe80b573e00683966803139
ELEKS-SOFTWARE\andrii.plakhtii <andrii.plakhtii@eleks.com>
2024-08-30 20:23:30 +0300

    upgrade migrations

--- DotNet8/Migrations/20240830165310_INIT.Designer.cs
+++ DotNet8/Migrations/20240830165310_INIT.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DotNet8.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20240830111551_rename")]
-    partial class rename
+    [Migration("20240830165310_INIT")]
+    partial class INIT
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
