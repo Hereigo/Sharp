12b32d1cfe433bd663582496d71c414eb1e65a1d
Hereigo <hereigo@online.ua>
2025-08-29 21:20:20 +0300

    Webhook added

--- DotNet8/Controllers/NotesController.cs
+++ DotNet8/Controllers/NotesController.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using Calendarium.Data;
+﻿using Calendarium.Data;
 using Calendarium.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Calendarium.Controllers
 {
