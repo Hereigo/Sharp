0fdb1ca8f5eeee9a9477bb2f960735d443f585e9
Hereigo <hereigo@online.ua>
2026-01-26 20:57:40 +0200

    update

---
+++ WebView2Starter/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace WebView2Starter
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            await WebView.EnsureCoreWebView2Async();
+
+            string htmlPath = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "wwwroot",
+                "index.html"
+            );
+
+            WebView.Source = new Uri(htmlPath);
+
+            // Handle messages from the web content
+            WebView.CoreWebView2.WebMessageReceived += (s, e) =>
+            {
+                string message = e.TryGetWebMessageAsString();
+                MessageBox.Show($"From Web: {message}");
+
+                // Send a message back to the web content
+                WebView.ExecuteScriptAsync(
+                    "alert('Hello from C#!')"
+                );
+            };
+        }
+    }
+}
