026b9c57919dac23dcd303857b3e17f747219a2d
Hereigo <hereigo@online.ua>
2026-02-09 16:52:05 +0200

    update

---
+++ Zastosunok/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Zastosunok
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.FlatStyle = FlatStyle.System;
+            label1.Font = new Font("Verdana", 18.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(9, 9);
+            label1.Margin = new Padding(0);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 31);
+            label1.TabIndex = 0;
+            label1.Text = "00";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.FlatStyle = FlatStyle.System;
+            label2.Font = new Font("Verdana", 18.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(59, 9);
+            label2.Margin = new Padding(0);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 31);
+            label2.TabIndex = 1;
+            label2.Text = "00";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.FlatStyle = FlatStyle.System;
+            label3.Font = new Font("Verdana", 18.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(109, 9);
+            label3.Margin = new Padding(0);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 31);
+            label3.TabIndex = 2;
+            label3.Text = "00";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(9, 45);
+            button1.Name = "button1";
+            button1.Size = new Size(66, 44);
+            button1.TabIndex = 3;
+            button1.Text = "Start";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(80, 45);
+            button2.Name = "button2";
+            button2.Size = new Size(66, 44);
+            button2.TabIndex = 4;
+            button2.Text = "Reset";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(158, 99);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Tool";
+            TopMost = true;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private Button button2;
+    }
+}
