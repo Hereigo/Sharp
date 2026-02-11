b817d2bd7d410faf2383afd93d54b39f6759e041
Hereigo <hereigo@online.ua>
2026-02-11 17:47:54 +0200

    update

--- Zastosunok/Form1.cs
+++ Zastosunok/Form1.cs
@@ -2,69 +2,33 @@ namespace Zastosunok;
 
 public partial class Form1 : Form
 {
-    private NotifyIcon trayIcon;
-    private System.Windows.Forms.Timer timer;
+    private readonly NotifyIcon trayIcon;
+    private readonly System.Windows.Forms.Timer timer;
 
     public Form1()
     {
         InitializeComponent();
 
-        // Create tray icon
-        trayIcon = new NotifyIcon();
-        trayIcon.Icon = this.Icon;
-        trayIcon.Visible = true;
-        trayIcon.Text = "My Application";
+        trayIcon = TrayIconCreate();
 
-        trayIcon.DoubleClick += TrayIcon_DoubleClick;
-
-        ContextMenuStrip menu = new ContextMenuStrip();
-        menu.Items.Add("Show", null, (s, e) => ShowForm());
-        menu.Items.Add("Exit", null, (s, e) => ExitApplication());
-        trayIcon.ContextMenuStrip = menu;
-
-        //labelSec.Text = "59";
-
-        timer = new System.Windows.Forms.Timer();
-        timer.Interval = 1000;
-        timer.Tick += Timer_Tick;
+        timer = new System.Windows.Forms.Timer { Interval = 1000 };
+        timer.Tick += TimerExecuteByInterval;
 
         // TEST
         timer.Start();
     }
 
-    protected override void OnFormClosing(FormClosingEventArgs e)
+    private void buttonStart_Click(object sender, EventArgs e)
     {
-        if (e.CloseReason == CloseReason.UserClosing)
-        {
-            e.Cancel = true;  // Cancel closing
-            this.Hide();
-        }
-        else
-        {
-            base.OnFormClosing(e);
-        }
+        if (!timer.Enabled) { timer.Start(); }
+        else { timer.Stop(); }
+    }
+    private void buttonReset_Click(object sender, EventArgs e)
+    {
+
     }
 
-    private void TrayIcon_DoubleClick(object sender, EventArgs e)
-    {
-        ShowForm();
-    }
-
-    private void ShowForm()
-    {
-        this.Show();
-        this.WindowState = FormWindowState.Normal;
-        this.BringToFront();
-    }
-
-    private void ExitApplication()
-    {
-        trayIcon.Visible = false; // Always set before exiting!!!
-        trayIcon.Dispose();
-        Application.Exit();
-    }
-
-    private void Timer_Tick(object sender, EventArgs e)
+    private void TimerExecuteByInterval(object? sender, EventArgs e)
     {
         var sec = int.TryParse(labelSec.Text, out int s) ? s : 0;
         var min = int.TryParse(labelMin.Text, out int m) ? m : 0;
@@ -83,16 +47,6 @@ public partial class Form1 : Form
             hour++;
         }
         labelMin.Text = min.ToString("D2");
-
-    }
-
-    private void buttonStart_Click(object sender, EventArgs e)
-    {
-        if (!timer.Enabled) { timer.Start(); }
-        else { timer.Stop(); }
-    }
-    private void buttonReset_Click(object sender, EventArgs e)
-    {
 
     }
 }
---
+++ Zastosunok/TrayIcon.cs
@@ -0,0 +1,56 @@
+﻿namespace Zastosunok;
+
+public partial class Form1
+{
+    private NotifyIcon TrayIconCreate()
+    {
+        // Create tray icon
+        var trayIcon = new NotifyIcon
+        {
+            Icon = this.Icon,
+            Visible = true,
+            Text = "Zastosunok"
+        };
+
+        trayIcon.DoubleClick += TrayIcon_DoubleClick;
+
+        ContextMenuStrip menu = new ContextMenuStrip();
+        menu.Items.Add("Show", null, (s, e) => ShowForm());
+        menu.Items.Add("Exit", null, (s, e) => ExitApplication());
+        trayIcon.ContextMenuStrip = menu;
+
+        return trayIcon;
+    }
+
+    protected override void OnFormClosing(FormClosingEventArgs e)
+    {
+        if (e.CloseReason == CloseReason.UserClosing)
+        {
+            e.Cancel = true;  // Cancel closing
+            this.Hide();
+        }
+        else
+        {
+            base.OnFormClosing(e);
+        }
+    }
+
+    private void TrayIcon_DoubleClick(object? sender, EventArgs e)
+    {
+        ShowForm();
+    }
+
+    private void ShowForm()
+    {
+        this.Show();
+        this.WindowState = FormWindowState.Normal;
+        this.BringToFront();
+    }
+
+    private void ExitApplication()
+    {
+        trayIcon.Visible = false; // Always set before exiting!!!
+        trayIcon.Dispose();
+        Application.Exit();
+    }
+}
